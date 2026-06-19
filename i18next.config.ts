98092324b862b5ae52faeda37ca027c92e73de6c
Daniil-SV <daniilsivacenko@gmail.com>
2026-06-19 20:13:27 +0300

    feat: Add more locales

--- i18next.config.ts
+++ i18next.config.ts
@@ -1,5 +1,5 @@
 export default {
-	locales: ["en-US"],
+	locales: ["en-US", "en-GB", "bg", "cs", "da", "de", "el", "es-ES", "es-419", "fi", "fr", "hi", "hr", "hu", "id", "it", "ja", "ko", "lt", "nl", "no", "pl", "pt-BR", "ro", "ru", "sv-SE", "th", "tr", "uk", "vi", "zh-CN", "zh-TW"],
 	extract: {
 		input: "source/**/*.{js,jsx,ts,tsx}",
 		output: "data/{{language}}/{{namespace}}.json",
